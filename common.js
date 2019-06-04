619c49e6531957669195702938c976fe36e2cfbc
Monica Dinculescu <noms@google.com>
2019-06-04 15:59:19 -0700

    add local country file with session counts

--- common.js
+++ common.js
@@ -1,7 +1,7 @@
 // Where to load the data from.
 const LABELS_URL = './melodyLabels.json';
 const TOP_OVERALL_URL = 'data/top_overall.json';
-const TOP_PER_COUNTRY_URL = 'https://cdn.glitch.com/b078d442-623b-41f0-a809-48f5f1ec1cbf%2Ftop_per_country.json?1559164216772';
+const TOP_PER_COUNTRY_URL = 'data/top_per_country.json';
 const DATAPOINTS_URL = 'https://cdn.glitch.com/b078d442-623b-41f0-a809-48f5f1ec1cbf%2Fdataset_samples.json?1559248203882';
 
 // Map delta interval -> color. This is how I generated it, but I'm saving it
