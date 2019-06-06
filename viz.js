15c4eea050e56a30211fe21518afb6a4a9e87739
Monica Dinculescu <noms@google.com>
2019-06-05 19:52:23 -0700

    label unseen melodies

--- viz.js
+++ viz.js
@@ -171,6 +171,13 @@ function showTooltip(d, el) {
   d.descendants().forEach(x => totalSessions += (x.data.sessions || 0));
   document.getElementById('sessionsText').textContent = totalSessions;
 
+  let isUnseen = false;
+  unseenSession.hidden = false;
+  d.descendants().forEach(x => {
+    if (x.data.unseen) isUnseen = true;
+  });
+  document.getElementById('unseenSession').hidden = !isUnseen;
+
   const tooltip = document.getElementById('tooltip');
   tooltip.removeAttribute('hidden');
 
