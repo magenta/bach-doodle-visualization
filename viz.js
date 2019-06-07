d28f7eed639fa44e464fd5b2453874f14a0d3da2
Monica Dinculescu <noms@google.com>
2019-06-07 10:33:56 -0700

    fix share it melody link

--- viz.js
+++ viz.js
@@ -245,10 +245,9 @@ function handleClick(d) {
 
   // So that we can hardlink.
   window.location.hash = d.elementIndex;
-  melodyTweetLink.href = 'https://twitter.com/intent/tweet?hashtags=madewithmagenta&text=' +
-  encodeURIComponent(`Listen to this melody from the Bach Doodle dataset! \
-https://meowni.ca/doodle-data-explorer/overall.html\#${d.elementIndex}`);
 
+  melodyTweetLink.href = 'https://twitter.com/intent/tweet?hashtags=madewithmagenta&text=' +
+  encodeURIComponent('Listen to this melody from the Bach Doodle dataset! ' + window.location.href);
   coucouLink.href = getCoucouLink();
 }
 
